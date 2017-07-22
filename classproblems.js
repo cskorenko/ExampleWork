0187cf0cca9ec070e173c2dc4e8650b979b64ebc
cskorenko <cskorenko@hotmail.com>
2017-07-22 09:14:51 -0500

    Update classproblems.js

--- classproblems.js
+++ classproblems.js
@@ -1,10 +1,10 @@
 for (let i =1; i <= 100; i++) {
-  if (i % 3 === 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i + "FizzBuzz")
+  } else if (i % 3 === 0) {
     console.log(i + "Fiz");
   } else if (i % 5 === 0) {
     console.log(i + "Buzz");
-  } else if (i % 3 === 0 && i % 5 === 0) {
-    console.log(i + "FizzBuzz");
   }
 }
 
