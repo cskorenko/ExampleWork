738e507c38c8032e6a25d2a0b1156d3834665434
Christine Skorenko <cskorenko@hotmail.com>
2017-09-10 17:34:24 -0500

    update

---
+++ Homework/project/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use('/', routes);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000:')
+});
+
+module.exports = app;
