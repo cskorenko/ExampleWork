c1f0c849c2746ce91f3ea6d97f5b5c0574fa8c5f
Christine Skorenko <cskorenko@hotmail.com>
2017-10-02 18:55:21 -0500

    class 10-2

--- In-Class/login/index.js
+++ In-Class/login/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const errMiddleware = require('./err-middleware')
 const helper = require('./helpers');
 const moment = require('moment');
 
@@ -10,7 +11,7 @@ app.get('/', (req, res) => {
   res.status(200).send('Welcome to my login app');
 });
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
   const user = {
     username: req.body.username,
     password: req.body.password
@@ -24,7 +25,9 @@ app.post('/', (req, res) => {
     const validUser = helper.validateUser(JSON.parse(results.body), user);
 
     if(!validUser) {
-      res.status(401).end();
+      // res.status(401).end();
+      throw new Error('Invalid User Credentials')
+      // next('Invalid User');
     } else if (validUser) {
       let todayDate = moment(Date.now()).format('YYYY-MM-DD');
       let urlTracking = `http://localhost:3002/tracking?username=${user.username}&currentDate=${todayDate}`
@@ -33,12 +36,16 @@ app.post('/', (req, res) => {
     }
   }).then((results) => {
     if(results.res.statusCode === 403) {
-      res.status(401).end()
+      throw new Error('Too Many Login Attemps');
     } else {
       res.status(200).json(user);
     }
-  })
+  }).catch((e) => {
+    next(e);
+  });
 });
+
+app.use(errMiddleware);
 
 app.listen(3003, () => {
   console.log('Listening on Port 3003')
